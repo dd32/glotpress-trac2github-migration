0a68ebd6d955429ff9227802ec5e041e405bd8ef
Dion Hulse <contact@dd32.id.au>
2022-11-09 11:10:55 +1000

    Document the config options.

--- config.php
+++ config.php
@@ -1,7 +1,11 @@
 <?php
 
+// Trac authentication if required for access to the TRAC_HOST
 define( 'USER', '' );
 define( 'PASS', '' );
+
 define( 'TRAC_HOST', 'glotpress.trac.wordpress.org' );
 define( 'GITHUB_REPO', 'dd32/glotpress-trac2github-migration-issues' );
-define( 'GITHUB_TOKEN', '' ); // PAT
+
+// PAT of a temporary user that we'll delete after the import, so that all created issues are owned by github.com/ghost.
+define( 'GITHUB_TOKEN', '' );
